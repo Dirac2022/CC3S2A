a7add4a1ec9f20ee744132368611303e9c85cf83
Dirac <daronmitchel@gmail.com>
2025-04-22 22:52:03 -0500

    Update belly_steps for handle time descriptions in english

--- Actividad7/belly_project/features/steps/belly_steps.py
+++ Actividad7/belly_project/features/steps/belly_steps.py
@@ -6,7 +6,7 @@ def convertir_palabra_a_numero(palabra):
     try:
         return int(palabra)
     except ValueError:
-        numeros = {
+        numeros_castellano = {
             "cero": 0, "uno": 1, "una":1, "dos": 2, "tres": 3, "cuatro": 4, "cinco": 5,
             "seis": 6, "siete": 7, "ocho": 8, "nueve": 9, "diez": 10, "once": 11,
             "doce": 12, "trece": 13, "catorce": 14, "quince": 15, "dieciséis": 16,
@@ -14,7 +14,23 @@ def convertir_palabra_a_numero(palabra):
             "treinta": 30, "cuarenta":40, "cincuenta":50, "sesenta":60, "setenta":70,
             "ochenta":80, "noventa":90, "media": 0.5
         }
-        return numeros.get(palabra.lower(), 0)
+        
+        numeros_ingles = {
+            "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
+            "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10, "eleven": 11,
+            "twelve": 12, "thirteen": 13, "fourteen": 14, "fifteen": 15, "sixteen": 16,
+            "seventeen": 17, "eighteen": 18, "nineteen": 19, "twenty": 20,
+            "thirty": 30, "forty": 40, "fifty": 50, "sixty": 60, "seventy": 70,
+            "eighty": 80, "ninety": 90, "half": 0.5
+        }
+        
+        palabra = palabra.lower()
+        if palabra in numeros_castellano:
+            return numeros_castellano[palabra]
+        elif palabra in numeros_ingles:
+            return numeros_ingles[palabra]
+        else:
+            raise ValueError(f"No se pudo convertir la palabra '{palabra}' a un número.")
 
 # Modificaremos para que reciba valores de tipo decimal
 @given('que he comido {cukes:g} pepinos')
@@ -24,30 +40,25 @@ def step_given_eaten_cukes(context, cukes):
 @when('espero {time_description}')
 def step_when_wait_time_description(context, time_description):
     time_description = time_description.strip('"').lower()
-    time_description = time_description.replace('y', ' ')
+    # Agregamos la condicion para manejar el caso "and"
+    time_description = time_description.replace(' y', '  ').replace('and', ' ')
     time_description = time_description.strip()
 
     # Manejar casos especiales como 'media hora'
-    if time_description == 'media hora':
+    if time_description == 'media hora' or time_description == 'half an hour':
         total_time_in_hours = 0.5
     else:
-        # Expresión regular para extraer horas y minutos
-        # pattern = re.compile(r'(?:(\w+)\s*horas?)?\s*(?:(\w+)\s*minutos?)?')
-        # Ahora para que maneje horas, minutos y segundos
-        pattern = re.compile(r'(?:(\w+)\s*horas?)?\s*(?:(\w+)\s*minutos?)?\s*(?:(\w+)\s*segundos?)?')
+        # Mejoramos la expresion regular para que contemple palabras en ingles
+        pattern = re.compile(r'(?:(\w+)\s*(?:horas?|hours?))?\s*(?:(\w+)\s*(?:minutos?|minutes?)?)?\s*(?:(\w+)\s*(?:segundos?|seconds?)?)?')
         match = pattern.match(time_description)
 
         if match:
             hours_word = match.group(1) or "0"
             minutes_word = match.group(2) or "0"
-            
-            # Capturamos los segundos
             seconds_word = match.group(3) or "0"
 
             hours = convertir_palabra_a_numero(hours_word)
             minutes = convertir_palabra_a_numero(minutes_word)
-            
-            # Convertimos los segundos a numero
             seconds =  convertir_palabra_a_numero(seconds_word)
 
             # Recalculamos el tiempo total en horas
